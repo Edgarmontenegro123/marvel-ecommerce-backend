eacd7aa20903f77a7ccdbe949c7eea1eef0bfba7
Edgar <edgarmontenegro321@gmail.com>
2025-06-18 21:08:41 -0300

    💾 Add favourites table and protected CRUD endpoints

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 export function verifyToken(req, res, next) {
     const header = req.headers['authorization'];
     const token = header && header.split(' ')[1];
+
     if (!token) return res.sendStatus(401);
 
     try {
